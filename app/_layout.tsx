12a7370f8952bbad920cbd9d3a30a0ac5c0b608a
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-08-08 20:51:32 +0530

    Refactor comments in _layout.tsx for clarity

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,12 +9,12 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
+
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
+ 
   initialRouteName: '(tabs)',
 };
 
